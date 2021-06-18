1929e8a336cfea030fad67d156a386f414633b07
Billy Braga <bbraga@progi.com>
2021-06-18 04:11:59 -0400

    Use AddHostedService

--- HostedServicesBug/Startup.cs
+++ HostedServicesBug/Startup.cs
@@ -31,10 +31,10 @@ namespace HostedServicesBug
             services.AddSwaggerGen(
                 c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "HostedServicesBug", Version = "v1"}); }
             );
-            services.AddSingleton<IHostedService, FakeHostedService1>();
-            services.AddSingleton<IHostedService, FakeHostedService2>();
-            services.AddSingleton<IHostedService, FakeHostedService3>();
-            services.AddSingleton<IHostedService, FakeHostedService4>();
+            services.AddHostedService<FakeHostedService1>();
+            services.AddHostedService<FakeHostedService2>();
+            services.AddHostedService<FakeHostedService3>();
+            services.AddHostedService<FakeHostedService4>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
